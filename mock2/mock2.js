97c70f08e1007dfb11cb1499c8cd3b7e326ea8e0
梅林的法杖 <530264127@qq.com>
2021-03-14 23:42:09 +0800

    jest mock

---
+++ mock2/mock2.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export const fetchData = () => {
+  return axios.get('/').then(res => res.data);
+}
+
+export const getNumber = () => {
+  return 123
+}
+
+// "(function(){return '123'})()"
